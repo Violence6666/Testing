90d9bee4f732fd06e67087a90296382257621a39
ivan <120242124+Violence6666@users.noreply.github.com>
2022-12-17 10:55:00 +0300

    Update main.cpp

--- strange_bot_c_plus/main.cpp
+++ strange_bot_c_plus/main.cpp
@@ -13,20 +13,20 @@ int main() // добавить ()
     string botName = "Your Brains"; // := не пишеться
     string userName, dayOfWeek;
 
-    сout << "Hello. I am " << botName << endl
-    сout << "What is your name?" << endl
+    cout << "Hello. I am " << botName << endl; // ;
+    cout << "What is your name?" << endl; // ;
     getline(cin, userName)
-    cout << "Hello " << userName << endl
-    cout << "What is weekday now?" << endl
+    cout << "Hello " << userName << endl; // ;
+    cout << "What is weekday now?" << endl; // ;
     getline(cin, dayOfWeek);
     if(dayOfWeek = "monday") {
-        cout << "Sunday is so far away!"\n;//Ошибка "Sunday is so far away!/n";
+        cout << "Sunday is so far away!\n";// ;
     }
     else if(dayOfWeek = "friday") { //elseif пишется раздельно 
-        cout < "Hurraaa!"\n;//"Hurraaa!\n"
+        cout < "Hurraaa!\n";
     }
     else {
-        cout < "Working day ... Hrr"\n;//"Working day ... Hrr\n"
+        cout < "Working day ... Hrr\n";
     }
     return 0;//Ошибка retyrn 0
 }
